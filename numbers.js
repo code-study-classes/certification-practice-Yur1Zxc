f11149614256c0092c36416bc5bee262853a2eaf
Yur1Zxc <159612389+Yur1Zxc@users.noreply.github.com>
2024-04-08 04:41:43 +0300

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,84 +1,66 @@
+// Задание 1
 function calculateDistance(x1, x2) {
-    return Math.abs(x2 - x1);
-  }
-  
-console.log(calculateDistance(3, 10));
-console.log(calculateDistance(-5, 5));
-export{calculateDistance}
+  return Math.abs(x2 - x1);
+}
 
+console.log(calculateDistance(3, 7));
+// Задание 2
 function calculateSegmentProduct(A, B, C) {
-    const lengthAC = Math.abs(C - A);
-    const lengthBC = Math.abs(B - C);
-    return lengthAC * lengthBC;
-  }
-  
-  console.log(calculateSegmentProduct(2, 8, 5));
-  export{calculateSegmentProduct}
-
-  function calculateKilobytes(fileSizeInBytes) {
-    return Math.floor(fileSizeInBytes / 1024);
-  }
-  
-  console.log(calculateKilobytes(2048));
-  export{calculateKilobytes}
-
-  function calculateSegments(lengthA, lengthB) {
-    return Math.floor(lengthA / lengthB);
-  }
-  
-  console.log(calculateSegments(10, 3));
-  export{calculateSegments}
-
-  function calculateDigitSum(twoDigitNumber) {
-    first_num = Math.floor(twoDigitNumber % 10);
-    second_num = Math.floor (twoDigitNumber / 10);
-    return Math.abs (first_num + second_num);
+  return Math.abs((A-C)*(B-C));
 }
 
-console.log(calculateDigitSum(27));
-export{calculateDigitSum}
-
-function swapHundredsAndTens(threeDigitNumber) {
-    const hundreds = Math.floor(threeDigitNumber / 100);
-    const tens = Math.floor((threeDigitNumber / 10) % 10);
-    const ones = threeDigitNumber % 10;
-    return tens * 100 + hundreds * 10 + ones;
+console.log(calculateSegmentProduct(0, 5, 2));
+// Задание 3
+function calculateKilobytes(fileSizeInBytes) {
+return Math.floor(fileSizeInBytes / 1024);
 }
 
-console.log(swapHundredsAndTens(456));
-export{swapHundredsAndTens}
+console.log(calculateKilobytes(5000));
+// Задание 4
+function calculateSegments(lengthA, lengthB) {
+  return Math.floor(lengthA/lengthB);
+}
 
+console.log(calculateSegments(10, 3))
+// Задание 5
+function calculateDigitSum(twoDigitNumber) {
+  let first_num = Math.floor(twoDigitNumber % 10)
+  let second_num = Math.floor(twoDigitNumber / 10)
+  return Math.abs(first_num + second_num);
+}
+
+console.log(calculateDigitSum(47));
+// Задание 6
+function swapHundredsAndTens(treeDigitNumber) {
+  let num_A = Math.floor(treeDigitNumber / 100)
+  let num_B = Math.floor((treeDigitNumber / 10) % 10)
+  let num_C = Math.floor(treeDigitNumber % 10)
+  return Math.abs(num_B * 100 + num_A * 10 + num_C);
+}
+console.log(swapHundredsAndTens(123));
+// Задание 7
 function getHundredsDigit(number) {
-    if (number > 999)
-    return Math.floor(number / 100) % 10;
+  if (number > 0);
+    return Math.floor(number / 100 % 10);
 }
-
-console.log(getHundredsDigit(1422));
-export{getHundredsDigit}
-
+console.log(getHundredsDigit(1234));
+// Задание 8
 function getFullHours(seconds) {
-    return Math.floor(seconds / 3600);
-    return Math.floor(seconds / 7200);
-    return Math.floor(seconds / 10800);
+return Math.floor(seconds / 60 / 60);
 }
-
 console.log(getFullHours(3600));
-export{getFullHours}
-
-function getDayOfWeek(dayOfYear) {
-    const firstDayOfWeek = 1;
-    const dayOfWeek = (firstDayOfWeek + dayOfYear - 1) % 7;
-    return dayOfWeek;
-  }
-  
-  console.log(getDayOfWeek(1));
-  export{getDayOfWeek}
-
-  function countSquares(A, B, C) {
-    const horizontalSquares = Math.floor(A / C);
-    const verticalSquares = Math.floor(B / C);
-    return horizontalSquares * verticalSquares;
-  }
-  
-  console.log(countSquares(4, 6, 2));
-  export{countSquares}
+// Задание 9 
+function getDayOfWeek(dayOFYear) {
+let firstDay = 1
+let weekDay = (firstDay + dayOFYear - 1) % 7
+return (weekDay);
+}
+console.log(getDayOfWeek(365));
+// Задание 10
+function countSquares(A, B, C) {
+let x = Math.floor(A / C)
+let y = Math.floor(B / C)
+return Math.floor( x * y);
+}
+console.log(countSquares(10, 10, 5));
+export { calculateDistance, calculateSegmentProduct, calculateKilobytes, calculateSegments, calculateDigitSum,  swapHundredsAndTens, getHundredsDigit, getFullHours, getDayOfWeek, countSquares };
